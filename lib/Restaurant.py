33973aa40538a90001b55a44946c59ea68bcc3ef
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-27 13:31:29 +0300

    Mission Passed

---
+++ lib/Restaurant.py
@@ -0,0 +1,7 @@
+class Restaurant:
+    def __init__(self, name=""):
+        self._name = name
+
+    def name(self,restaurant_name):
+        restaurant_name = self._name
+        return restaurant_name
