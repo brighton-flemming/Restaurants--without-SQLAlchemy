ced8808b120e141b5a15b83113f7152fe25c6825
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-27 18:05:13 +0300

    Mission Passed

--- lib/Restaurant.py
+++ lib/Restaurant.py
@@ -1,27 +1,22 @@
 
 from Review import Review
 class Restaurant:
-    def __init__(self, name,review_id, restaurant_id, customer_id):
+    def __init__(self, name, restaurant_id):
         self._name = str(name)
-        self.review_id = review_id
+        # self.review_id = review_id
         self.restaurant_id = restaurant_id
-        self.customer_id = customer_id
+        # self.customer_id = customer_id
         self.reviews = []
 
-    def name(self,restaurant_name):
-        restaurant_name = self._name
-        return restaurant_name
+    def get_name(self):
+        return self._name
     
-    def customers(restaurant_id, reviews):
-        restaurant_reviews = [review for review in reviews if review.restaurant_id == restaurant_id]
-        unique_customers = set(reviews.customer_id for review in restaurant_reviews)
-
+    def get_customers(self):
+        unique_customers = set(review.customer_id for review in self.reviews)
         return list(unique_customers)
 
-    def reviews(self):
-        review_list = []
-        review_list.append(Review.all())
-        return review_list
+    def get_reviews(self):
+        return self.reviews
     
     def average_star_rating(self):
         if not self.reviews:
@@ -32,24 +27,36 @@ class Restaurant:
         return average_rating
     
 reviews_data = [
-    Restaurant.customers(1, "restaurant_A, customer_1"),
-     Restaurant.customers(2, "restaurant_B, customer_2"),
-      Restaurant.customers(3, "restaurant_C, customer_3"),
-       Restaurant.customers(4, "restaurant_D, customer_4"),
-        Restaurant.customers(5, "restaurant_E, customer_5"),
+    Review("customer_1","restaurant_A", 4),
+     Review("customer_2","restaurant_A", 5),
+      Review("customer_3","restaurant_A", 3),
+       Review("customer_4","restaurant_A", 2),
+        Review("customer_5","restaurant_A", 5),
+    
 ]
 
     
-restaurants_data = [
-   Restaurant("restaurant_A","Restaurant A"),
-   Restaurant("restaurant_B","Restaurant B"),
-   Restaurant("restaurant_C","Restaurant C"),
-   Restaurant("restaurant_D","Restaurant D"),
-   Restaurant("restaurant_E","Restaurant E"),
-]
+restaurant_A = Restaurant("restaurant_A","Restaurant A"),
+restaurant_B = Restaurant("restaurant_B","Restaurant B"),
+restaurant_C = Restaurant("restaurant_C","Restaurant C"),
+restaurant_D = Restaurant("restaurant_D","Restaurant D"),
+restaurant_E = Restaurant("restaurant_E","Restaurant E"),
 
-avg_rating = restaurants_data[0].average_star_rating()
-print(f"Average Rating for {restaurants_data[0].name} : {avg_rating:.2f}")
+for review in reviews_data:
+    if review.restaurant_id == "restaurant_A":
+        restaurant_A.reviews.append(review)
+    elif  review.restaurant_id == "restaurant_B":
+         restaurant_B.reviews.append(review)
+    elif  review.restaurant_id == "restaurant_C":
+         restaurant_C.reviews.append(review)
+    elif  review.restaurant_id == "restaurant_D":
+         restaurant_D.reviews.append(review)
+    elif  review.restaurant_id == "restaurant_E":
+         restaurant_E.reviews.append(review)
+      
+
+avg_rating_A = restaurant_A.average_star_rating()
+print(f"Average Rating for {restaurant_A.get_name} : {avg_rating_A:.2f}")
 
    
         
