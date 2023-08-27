a5121d6202dd43da925cd82d75ead4a1870b695f
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-27 17:48:02 +0300

    Mission Passed

--- lib/Review.py
+++ lib/Review.py
@@ -1,26 +1,38 @@
-
 from Customer import Customer
 from Restaurant import Restaurant
+
 class Review:
     def __init__(self, customer, restaurant, rating) :
         self._customer =  customer
         self._restaurant = restaurant
         self._rating = int(rating)
 
-    def rating(self):
+    def get_rating(self):
         return self._rating
     
-    def all(self, rating):
+    def get_all_ratings(self):
         rating_list = []
         rating = self._rating
         rating_list.append(rating)
         return rating_list
     
-    def customer(self):
-        self._customer = Customer
+    def get_customer(self):
         return self._customer
     
-    def restaurant(self):
-        self._restaurant = Restaurant
+    def get_restaurant(self):
         return self._restaurant
     
+customer_instance =  Customer("customer_id","John Doe")
+restaurant_instance = Restaurant("restaurant_id", "Restaurant_A")
+review_instance = Review(customer_instance, restaurant_instance, 4)
+
+rating = review_instance.get_rating()
+print("Rating: ", rating)
+
+all_ratings = review_instance.get_all_ratings()
+print("All Ratings: ", all_ratings)
+
+customer = review_instance.get_customer()
+restaurant = review_instance.get_restaurant()
+print("Customer: ", customer)
+print("Restaurant: ", restaurant)
