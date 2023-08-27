30712051477d2b2ca9a215918be6fae14be22b89
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-27 13:19:31 +0300

    Mission Passed

---
+++ lib/Customer.py
@@ -0,0 +1,21 @@
+class Customer:
+    def __init__(self, first_naame, family_name):
+     self._first_name = first_naame
+     self._family_name = family_name
+
+    def given_name(self):
+       given_name = self._first_name
+       return given_name
+     
+    def family_name(self):
+      return self._family_name
+     
+    def change_family_name(self, new_family_name):
+       self._family_name = new_family_name
+
+    def full_name(self, person_name):
+        person_name = print(f"{self.given_name}  {self.family_name}")
+        return person_name
+    
+    def all(self):
+       return all(Customer.full_name)
